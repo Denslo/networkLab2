3ccdecd6df67811796aba91c67f33f19b3db2685
shai <shaiambar@yahoo.com>
2014-01-16 22:25:39 +0200

    ad

--- src/SMTPNotifictionThread.java
+++ src/SMTPNotifictionThread.java
@@ -1,14 +1,6 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Map.Entry;
+
 import java.util.Calendar;
-import java.util.Properties;
+
 
 public class SMTPNotifictionThread implements Runnable {
 
@@ -18,79 +10,70 @@ public class SMTPNotifictionThread implements Runnable {
 
 	@Override
 	public void run() {
-		//while (true) {
-			Calendar cal = Calendar.getInstance();
+		// while (true) {
+		Calendar cal = Calendar.getInstance();
 
-			checkReminder(cal);
-			checkTask(cal);
-			//checkPolls(cal);
+		checkReminder(cal);
+		checkTask(cal);
+		// checkPolls(cal);
 
-		//	 TODO Make Delay
-		//}
+		// TODO Make Delay
+		// }
 
 	}
-/*
-	private void checkPolls(Calendar cal) {
-		Polls polls;
-		Properties pollsProp = new Properties();
 
-		try {
-			pollsProp.load(pollsFile);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		for (Entry<Object, Object> entry : pollsProp.entrySet()) {
-			polls = new Polls((String) entry.getKey(),(String) entry.getValue());
-			
-			if (!polls.isWas_handled()){
-				if (cal.after(polls.getDue_date())) {
-					//TODO something
-					
-				} else if (polls.isCompleted()) {
-					//TODO sendToCreatorPollComple(polls);
-					polls.setWas_handled(true);
-					
-				} else if (checkIfComplet(polls.getRecipient())) {
-					//TODO sendToCreatorPollComple(polls);
-					polls.setWas_handled(true);
-					polls.setCompleted(true);
-					
-				}	
-			}
-		}
-	}
-	*/
-	private boolean checkIfComplet(Recipient[] recipient) {
-		
+	/*
+	 * private void checkPolls(Calendar cal) { Polls polls; Properties pollsProp
+	 * = new Properties();
+	 * 
+	 * try { pollsProp.load(pollsFile); } catch (IOException e1) { // TODO
+	 * Auto-generated catch block e1.printStackTrace(); } for (Entry<Object,
+	 * Object> entry : pollsProp.entrySet()) { polls = new Polls((String)
+	 * entry.getKey(),(String) entry.getValue());
+	 * 
+	 * if (!polls.isWas_handled()){ if (cal.after(polls.getDue_date())) { //TODO
+	 * something
+	 * 
+	 * } else if (polls.isCompleted()) { //TODO sendToCreatorPollComple(polls);
+	 * polls.setWas_handled(true);
+	 * 
+	 * } else if (checkIfComplet(polls.getRecipient())) { //TODO
+	 * sendToCreatorPollComple(polls); polls.setWas_handled(true);
+	 * polls.setCompleted(true);
+	 * 
+	 * } } } }
+	 */
+ /*	private boolean checkIfComplet(Recipient[] recipient) {
+
 		for (Recipient recipient2 : recipient) {
-			if(!recipient2.isDidReply()){
+			if (!recipient2.isDidReply()) {
 				return false;
 			}
-			
+
 		}
 		return true;
-	}
+	}*/
 
 	private void checkTask(Calendar cal) {
-		
+
 		Task[] tasks = DBHandler.getAllTasks();
-		
+
 		for (Task task : tasks) {
 
 			if (!task.isWas_handled()) {
-				
+
 				if (!(task.isTasKFirstSent())) {
 					// TODO sendTaskToRecipient(task);
 					task.setFirstsent(true);
 				}
-				
-				if(task.isTaskDone()){
-					//TODO sendTaskCompleted(task);
+
+				if (task.isTaskDone()) {
+					// TODO sendTaskCompleted(task);
 					task.setWas_handled(true);
-					
-				}else if (cal.after(task.getDue_date())) {
-					// TODO sendTaskTimeIsDue(task.getCreator(),task.getRecipient()[0]);
+
+				} else if (cal.after(task.getDue_date())) {
+					// TODO
+					// sendTaskTimeIsDue(task.getCreator(),task.getRecipient()[0]);
 					task.setWas_handled(true);
 				}
 			}
@@ -100,7 +83,7 @@ public class SMTPNotifictionThread implements Runnable {
 	private void checkReminder(Calendar cal) {
 
 		Reminder[] reminders = DBHandler.getAllRiminders();
-				
+
 		for (Reminder reminder : reminders) {
 
 			if (!reminder.isWas_handled()) {
